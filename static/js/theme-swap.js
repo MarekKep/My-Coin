75804345972ccf7551fd72fd4d76603086a8676a
Marek Kepsha <marek.kepsha@gmail.com>
2022-08-05 16:20:09 +0300

    some changes

--- static/js/theme-swap.js
+++ static/js/theme-swap.js
@@ -3,11 +3,13 @@ let pastel = document.querySelector('#pastel');
 let city = document.querySelector('#city');
 
 dark.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('img/dark-theme.jpg')";
+    document.body.style.backgroundImage = 'url("https://cdn.wallpapersafari.com/5/24/IvSYOt.jpg")';
+    document.body.style.color = "white";
 });
 
 pastel.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('img/pastel.jpg')";
+    document.body.style.backgroundImage = "url('https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000')";
+    document.body.style.backgroundSize = "cover";
 });
 
 city.addEventListener('click', () =>{
