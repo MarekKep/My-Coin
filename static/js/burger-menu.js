00ba195f9bcc1037ecfdfe4b5435d608b29525e9
Marek Kepsha <marek.kepsha@gmail.com>
2022-08-06 16:09:10 +0300

    deleted some items

--- static/js/burger-menu.js
+++ static/js/burger-menu.js
@@ -11,21 +11,3 @@ document.querySelector(".nav-link").forEach(n => n.addEventListener("click", () 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
-
-function oddEvenNumber(num) {
-    var str = num.toString();
-    var dict = {
-      odd: 0,
-      even: 0
-      };
-    for (let i = 0; i < str.length; i++) {
-        if (parseInt(str.slice(i, i+1), 10) % 2 === 0) {
-            dict["even"] += 1;
-        }
-        else{
-            dict["odd"] += 1;
-        }
-}
-    return dict;
-}
-console.log(oddEvenNumber(100500));
